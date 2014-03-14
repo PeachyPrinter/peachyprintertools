2f08528a2edf9dd8d5d3a055fa02a8cfd7635669
james townley <james@townley.ca>
2014-03-14 14:41:26 -0600

    Trying to add shortcuts for windows

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,28 @@ from VERSION import version
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = ['domain','infrastructure'], excludes = [])
+shortcut_table = [
+    ("PeachyPrinterTools",                  # Shortcut
+     "DesktopFolder",                       # Directory_
+     "Peachy Printer Tools",                # Name
+     "TARGETDIR",                           # Component_
+     "[TARGETDIR]PeachyPrinterTools.exe",   # Target
+     None,                                  # Arguments
+     None,                                  # Description
+     None,                                  # Hotkey
+     None,                                  # Icon
+     None,                                  # IconIndex
+     None,                                  # ShowCmd
+     'TARGETDIR'                            # WkDir
+     )
+    ]
+
+buildOptions = { 
+        'packages' : ['domain','infrastructure'], 
+        'excludes' : [],
+        }
+
+bdist_msi_options = dict(buildOptions.items(), data = { 'Shortcut' : shortcut_table })
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
@@ -15,5 +36,5 @@ executables = [
 setup(name='Peachy Printer Tools',
       version = version,
       description = 'Tool Set for calibrating the Peachy Printer and printing models',
-      options = dict(build_exe = buildOptions),
+      options =  { 'build_exe' : buildOptions, "bdist_msi": bdist_msi_options, },
       executables = executables)
