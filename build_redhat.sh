469534606f269e119c0dea51811c91059c5b2df2
James Townley <software@peachyprinter.com>
2014-04-02 16:29:25 -0600

    Made a red hat specific build file

---
+++ build_redhat.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+echo "------------------------------------"
+echo "Cleaning workspace"
+echo "------------------------------------"
+
+# TODO JT 2014-02-13 - Should clean the workspace
+rm -rf src/build
+rm -rf *.dmg
+rm -f src/VERSION.py
+rm -f version.properties 
+
+echo "------------------------------------"
+echo "Extracting Git Revision Number"
+echo "------------------------------------"
+
+SEMANTIC=`cat symantic.version`
+
+function trim() { echo $1; }
+
+if [ -z $GIT_HOME ]; then
+  if [ -f "/usr/local/bin/git" ]; then
+    export GIT_HOME=/usr/local/bin/git
+  elif [ -f "/usr/bin/git" ]; then
+    export GIT_HOME=/usr/bin/git
+  elif [ -f "/bin/git" ]; then
+    export GIT_HOME=/bin/git
+  else
+    echo "Could not find git."
+    exit 1
+  fi
+fi
+
+export GIT_REV_COUNT_RAW=`$GIT_HOME log --pretty=oneline | wc -l`
+export GIT_REV_COUNT=`trim $GIT_REV_COUNT_RAW`
+export GIT_REV=`$GIT_HOME rev-parse HEAD`
+
+VERSION=$SEMANTIC.$TAG$GIT_REV_COUNT
+echo "Version: $VERSION"
+echo "# THIS IS A GENERATED FILE " > version.properties
+echo "version='$VERSION'" >> version.properties
+echo "revision='$GIT_REV'" >> version.properties
+echo "Git Revision Number is $GIT_REV_COUNT"
+cp version.properties src/VERSION.py
+
+echo "------------------------------------"
+echo "Running Tests"
+echo "------------------------------------"
+
+python test/test-all.py
+
+if [ $? != 0 ]; then
+    echo "FAILED TESTS ABORTING"
+    exit 55
+fi
+
+echo "------------------------------------"
+echo "Create Peachy Tool Chain app"
+echo "------------------------------------"
+
+cd src
+python setup.py bdist_rpm
+if [ $? != 0 ]; then
+    echo "Packaging Failed Aborting"
+    exit 55
+fi
+cd ..
+mv src/dist/*.rpm .
