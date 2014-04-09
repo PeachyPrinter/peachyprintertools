b98e232e3b915b933fe8cc9f4e74aca246f699b0
James Townley <software@peachyprinter.com>
2014-04-09 08:17:30 -0600

    Adding api reference

---
+++ src/api/print_api.py
@@ -0,0 +1,14 @@
+
+class PrintAPI(object):
+    def __init__():
+        pass
+
+    def print_gcode(printer_name, g_code_file_like_object):
+        pass
+
+    def print_status():
+        return {'line': 1, 'last_message' : 'some message', 'etc': 'etc'}
+
+    def verify_gcode(printer_name, g_code_file_like_object):
+        # returns list/first errors and line numbers
+        pass
