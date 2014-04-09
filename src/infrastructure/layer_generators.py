558853bab689938af8349d160bb4b685f1bff4c5
James Townley <software@peachyprinter.com>
2014-04-09 16:55:51 -0600

    Added a single point generator for testing calibration

--- src/infrastructure/layer_generators.py
+++ src/infrastructure/layer_generators.py
@@ -1,3 +1,5 @@
+
+from domain.commands import *
 from domain.layer_generator import LayerGenerator
 
 class StubLayerGenerator(LayerGenerator):
@@ -8,3 +10,16 @@ class StubLayerGenerator(LayerGenerator):
         if len(self._layers) == 0:
             raise StopIteration()
         return self._layers.pop(0)
+
+class SinglePointGenerator(LayerGenerator):
+    def __init__(self, starting_xy = [0.0,0.0]):
+        self.xy = starting_xy
+        self.speed = 1
+
+    def set(self,xy):
+        self.xy = xy
+
+    def next(self):
+        layer = Layer(0.0)
+        layer.commands.append(LateralDraw(self.xy,self.xy,self.speed))
+        return layer
---
+++ test/infrastructure_test/layer_generators_test.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import sys
+from mock import patch
+
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
+
+from infrastructure.layer_generators import SinglePointGenerator
+from domain.commands import *
+import test_helpers
+
+class SinglePointGeneratorTests(unittest.TestCase,test_helpers.TestHelpers):
+    def test_can_call_next_and_get_specified_command(self):
+        layer_generator = SinglePointGenerator([0.0,0.0])
+        expected = Layer(0.0, commands = [LateralDraw([0.0,0.0],[0.0,0.0],1)])
+        actual = layer_generator.next()
+        self.assertLayerEquals(expected,actual)
+
+    def test_can_call_next_after_updating_point(self):
+        layer_generator = SinglePointGenerator([0.0,0.0])
+        expected = Layer(0.0, commands = [LateralDraw([1.0,1.0],[1.0,1.0],1)])
+        layer_generator.next()
+        layer_generator.set([1.0,1.0])
+        actual = layer_generator.next()
+        self.assertLayerEquals(expected,actual)
