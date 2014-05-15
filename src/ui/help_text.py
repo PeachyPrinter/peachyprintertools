8ea1700eaab9ee5477f5c2d74bc925f9e865b8ee
NathanGrayston <marketing@peachyprinter.com>
2014-05-15 11:13:25 -0600

    Fixed grammatical and spelling mistakes.

--- src/ui/help_text.py
+++ src/ui/help_text.py
@@ -1,18 +1,18 @@
-options_help = '''These are general setting for the peachy printer it is strongly recommended that if you change these values you re-calibrate the printer.\n
-Spot Diameter (mm) [0.5] - <b>Sets</b> the current size of the laser, 0.5 is a good default for peachy printers.\n
-Sub Layer Height (mm) [0.05] - Sublayers are added to your model when the vertical resolution of the model is no fine enough. 0.05 works out to about 600dpi\n
+options_help = '''These are general settings for the Peachy Printer.  It is strongly recommended that if you change these values you re-calibrate the printer.\n
+Spot Diameter (mm) [0.5] - <b>Sets</b> the current size of the laser beam, 0.5 is a good default for the Peachy Printer.\n
+Sub Layer Height (mm) [0.05] - Sublayers are added to your model when the vertical resolution of the model is not fine enough. 0.05mm works out to about 600dpi.\n
 Maximum Lead Distance (mm) [0.5] - If you are dripping too fast this is the distance ahead at which the printer will start skipping sublayers.'''
 
-calibration_help = '''Using the "Center Point" option align you grid paper\n
-Using Alignment align your grid paper with the laser line\n
-Calibration consists of 2 samples at to heights. Go to the top point and click through fields and records the real world examples repeat for the lower height\n
+calibration_help = '''Using the "Center Point" option align your grid paper\n
+Using Alignment align your grid paper with the laser line.\n
+Calibration consists of 2 samples at two heights. Go to the top point, click through the fields, and record the real world examples.  Repeat for the lower height.\n
 Use the calibrated Patterns to test things out.'''
 
 setup_audio_help = '''Some help setting up audio goes here'''
 
-setup_drip_calibration_help = '''Adjust the microphone input opn you computer to make the drips count correctly\n
-enter the End Height that you will wait for the drips to get to\n
+setup_drip_calibration_help = '''Adjust the microphone input on your computer to make the drips count correctly.\n
+enter the End Height that you will wait for the drips to get to.\n
 When your drips get to your end height click mark. \n
 Click save to save your chanages'''
 
-cure_test_help = '''Help setting up and runnig a cure test goes here'''
+cure_test_help = '''Help setting up and running a cure test goes here'''
