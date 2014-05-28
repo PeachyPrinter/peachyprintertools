f38c00ae1e6d8b7c8334ec82f5bd87930f9ae0ee
James Townley <software@peachyprinter.com>
2014-05-28 14:51:46 -0600

    Added new config and config options

--- src/infrastructure/configuration.py
+++ src/infrastructure/configuration.py
@@ -27,19 +27,119 @@ class OptionsConfiguration(object):
         else:
             raise ValueError("Draw Speed must be of %s" % (str(_type)))
 
+    @property
+    def laser_offset(self):
+        return self._laser_offset
+
+    @laser_offset.setter
+    def laser_offset(cls, value):
+        _type = types.ListType
+        _inner_type = types.FloatType
+        if (type(value) == _type and 
+            len(value) == 2 and 
+            type(value[0]) == _inner_type and
+            type(value[1]) == _inner_type):
+            cls._laser_offset = value
+        else:
+            raise ValueError("Laser Offset must be of %s" % (str(_type)))
+
+    @property
+    def sublayer_height_mm(self):
+        return self._sublayer_height_mm
+
+    @sublayer_height_mm.setter
+    def sublayer_height_mm(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._sublayer_height_mm = value
+        else:
+            raise ValueError("Sublayer Height must be of %s" % (str(_type)))
+
+    @property
+    def laser_thickness_mm(self):
+        return self._laser_thickness_mm
+
+    @laser_thickness_mm.setter
+    def laser_thickness_mm(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._laser_thickness_mm = value
+        else:
+            raise ValueError("Laser Thickness must be of %s" % (str(_type)))
+
 
 
 class DripperConfiguration(object):
     def __init__(self):
-        self.max_lead_distance_mm = None
-        self.drips_per_mm = None
+        self._max_lead_distance_mm = None
+        self._drips_per_mm = None
+
+    @property
+    def max_lead_distance_mm(self):
+        return self._max_lead_distance_mm
+
+    @max_lead_distance_mm.setter
+    def max_lead_distance_mm(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._max_lead_distance_mm = value
+        else:
+            raise ValueError("Max Lead distance must be of %s" % (str(_type)))
+
+    @property
+    def drips_per_mm(self):
+        return self._drips_per_mm
+
+    @drips_per_mm.setter
+    def drips_per_mm(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._drips_per_mm = value
+        else:
+            raise ValueError("Drips per mm must be of %s" % (str(_type)))
 
 
 class CalibrationConfiguration(object):
     def __init__(self):
         self._scale = None
         self._data = None
-        self.max_deflection = None
+        self._max_deflection = None
+
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._scale = value
+        else:
+            raise ValueError("Scale must be of %s" % (str(_type)))
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(cls, value):
+        _type = types.DictType
+        if type(value) == _type:
+            cls._data = value
+        else:
+            raise ValueError("Data must be of %s" % (str(_type)))
+
+    @property
+    def max_deflection(self):
+        return self._max_deflection
+
+    @max_deflection.setter
+    def max_deflection(cls, value):
+        _type = types.FloatType
+        if type(value) == _type:
+            cls._max_deflection = value
+        else:
+            raise ValueError("Max Deflection must be of %s" % (str(_type)))
 
 
 class SerialZAxisConfiguration(object):
@@ -49,11 +149,84 @@ class SerialZAxisConfiguration(object):
         self._on_command = None
         self._off_command = None
 
+    @property
+    def on(self):
+        return self._on
+
+    @on.setter
+    def on(cls, value):
+        _type = types.BooleanType
+        if type(value) == _type:
+            cls._on = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._port = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+    @property
+    def on_command(self):
+        return self._on_command
+
+    @on_command.setter
+    def on_command(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._on_command = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+    @property
+    def off_command(self):
+        return self._off_command
+
+    @off_command.setter
+    def off_command(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._off_command = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+   
 
 class AudioInputConfiguration(object):
     def __init__(self):
         self._bit_depth = None
         self._sample_rate = None
+
+    @property
+    def bit_depth(self):
+        return self._bit_depth
+
+    @bit_depth.setter
+    def bit_depth(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._bit_depth = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @sample_rate.setter
+    def sample_rate(cls, value):
+        _type = types.IntType
+        if type(value) == _type:
+            cls._sample_rate = value
+        else:
+            raise ValueError("Sample Rate must be of %s" % (str(_type)))
 
 
 class AudioOutputConfiguration(object):
@@ -62,6 +235,56 @@ class AudioOutputConfiguration(object):
         self._sample_rate = None
         self._modulation_on_frequency = None
         self._modulation_off_frequency = None
+
+    @property
+    def bit_depth(self):
+        return self._bit_depth
+
+    @bit_depth.setter
+    def bit_depth(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._bit_depth = value
+        else:
+            raise ValueError("Bit depth must be of %s" % (str(_type)))
+
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @sample_rate.setter
+    def sample_rate(cls, value):
+        _type = types.IntType
+        if type(value) == _type:
+            cls._sample_rate = value
+        else:
+            raise ValueError("Sample Rate must be of %s" % (str(_type)))
+
+
+    @property
+    def modulation_on_frequency(self):
+        return self._modulation_on_frequency
+
+    @modulation_on_frequency.setter
+    def modulation_on_frequency(cls, value):
+        _type = types.IntType
+        if type(value) == _type:
+            cls._modulation_on_frequency = value
+        else:
+            raise ValueError("Modulation On Frequency must be of %s" % (str(_type)))
+
+
+    @property
+    def modulation_off_frequency(self):
+        return self._modulation_off_frequency
+
+    @modulation_off_frequency.setter
+    def modulation_off_frequency(cls, value):
+        _type = types.IntType
+        if type(value) == _type:
+            cls._modulation_off_frequency = value
+        else:
+            raise ValueError("Sample Rate must be of %s" % (str(_type)))
     
 
 class AudioConfiguration(object):
@@ -70,22 +293,52 @@ class AudioConfiguration(object):
         self._output = AudioOutputConfiguration()
 
     @property
-    def input():
+    def input(self):
         return self._input
 
     @property
-    def output():
+    def output(self):
         return self._output
 
 class Configuration(object):
     def __init__(self):
         self._name = "Peachy Printer"
         self._audio = AudioConfiguration()
-        self._serial = SerialConfiguration()
+        self._serial = SerialZAxisConfiguration()
         self._calibration = CalibrationConfiguration()
         self._dripper = DripperConfiguration()
         self._options = OptionsConfiguration()
 
+    @property
+    def audio(self):
+        return self._audio
+
+    @property
+    def serial(self):
+        return self._serial
+    
+    @property
+    def calibration(self):
+        return self._calibration
+
+    @property
+    def dripper(self):
+        return self._dripper
+
+    @property
+    def options(self):
+        return self._options
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(cls, value):
+        _type = types.StringType
+        if type(value) == _type:
+            cls._name = value
+        else:
+            raise ValueError("Name must be of %s" % (str(_type)))
 
 
 class FileBasedConfigurationManager(ConfigurationManager):
--- test/infrastructure_test/configuration_test.py
+++ test/infrastructure_test/configuration_test.py
@@ -12,15 +12,142 @@ sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
 
 from infrastructure.configuration import FileBasedConfigurationManager as ConfigurationManager
-from infrastructure.configuration import OptionsConfiguration
+from infrastructure.configuration import Configuration
 import test_helpers
 
 class ConfigurationTests(unittest.TestCase,test_helpers.TestHelpers):
     def test_can_set_and_get_values(self):
-        expected_speed = 7.0
-        c = OptionsConfiguration()
-        c.draw_speed = expected_speed
-        self.assertEquals(expected_speed, c.draw_speed)
+        expected_name = "PP"
+        expected_output_bit_depth = "16"
+        expected_output_sample_frequency =  48000
+        expected_on_modulation_frequency = 8000
+        expected_off_modulation_frequency = 2000
+        expected_input_bit_depth = "8"
+        expected_input_sample_frequency = 4800
+        expected_sublayer_height_mm = 0.
+        expected_laser_thickness_mm = 0.1
+        expected_drips_per_mm = 10.1
+        expected_max_deflection = 75.2
+        expected_calibration_data = {
+            'height': 1.0 , 
+            'lower_points': { (1.0, 1.0):( 1.0,  1.0), (0.0, 1.0):(-1.0,  1.0), (1.0, 0.0):( 1.0, -1.0), (0.0, 0.0):(-1.0, -1.0) },
+            'upper_points': { (1.0, 1.0):( 1.0,  1.0), (0.0, 1.0):(-1.0,  1.0), (1.0, 0.0):( 1.0, -1.0), (0.0, 0.0):(-1.0, -1.0) },
+            }
+        expected_calibration_scale = 0.1 
+        expected_draw_speed = 2.0
+        expected_max_lead_distance_mm = 0.2
+        expected_use_serial_zaxis = True
+        expected_serial_port = "COM2"
+        expected_serial_on = "12"
+        expected_serial_off = "13"
+        expected_laser_offset = [ 0.1, 0.1]
+        config = Configuration()
+
+        config.name                                 = expected_name
+        config.audio.output.bit_depth               = expected_output_bit_depth
+        config.audio.output.sample_rate             = expected_output_sample_frequency
+        config.audio.output.modulation_on_frequency = expected_on_modulation_frequency
+        config.audio.output.modulation_off_frequency= expected_off_modulation_frequency
+        config.audio.input.bit_depth                = expected_input_bit_depth
+        config.audio.input.sample_rate              = expected_input_sample_frequency
+        config.options.sublayer_height_mm           = expected_sublayer_height_mm
+        config.options.laser_thickness_mm           = expected_laser_thickness_mm
+        config.dripper.drips_per_mm                 = expected_drips_per_mm
+        config.calibration.max_deflection           = expected_max_deflection
+        config.calibration.data                     = expected_calibration_data
+        config.calibration.scale                    = expected_calibration_scale
+        config.options.draw_speed                   = expected_draw_speed
+        config.dripper.max_lead_distance            = expected_max_lead_distance_mm
+        config.serial.on                            = expected_use_serial_zaxis
+        config.serial.port                          = expected_serial_port
+        config.serial.on_command                    = expected_serial_on
+        config.serial.off_command                   = expected_serial_off
+        config.options.laser_offset                 = expected_laser_offset
+
+        self.assertEquals(expected_name, config.name )
+        self.assertEquals(expected_output_bit_depth, config.audio.output.bit_depth )
+        self.assertEquals(expected_output_sample_frequency, config.audio.output.sample_rate )
+        self.assertEquals(expected_on_modulation_frequency, config.audio.output.modulation_on_frequency )
+        self.assertEquals(expected_off_modulation_frequency, config.audio.output.modulation_off_frequency )
+        self.assertEquals(expected_input_bit_depth, config.audio.input.bit_depth )
+        self.assertEquals(expected_input_sample_frequency, config.audio.input.sample_rate )
+        self.assertEquals(expected_sublayer_height_mm, config.options.sublayer_height_mm )
+        self.assertEquals(expected_laser_thickness_mm, config.options.laser_thickness_mm )
+        self.assertEquals(expected_drips_per_mm, config.dripper.drips_per_mm )
+        self.assertEquals(expected_max_deflection, config.calibration.max_deflection )
+        self.assertEquals(expected_calibration_data, config.calibration.data )
+        self.assertEquals(expected_calibration_scale, config.calibration.scale )
+        self.assertEquals(expected_draw_speed, config.options.draw_speed )
+        self.assertEquals(expected_max_lead_distance_mm, config.dripper.max_lead_distance )
+        self.assertEquals(expected_use_serial_zaxis, config.serial.on )
+        self.assertEquals(expected_serial_port, config.serial.port )
+        self.assertEquals(expected_serial_on, config.serial.on_command )
+        self.assertEquals(expected_serial_off, config.serial.off_command )
+        self.assertEquals(expected_laser_offset, config.options.laser_offset )
+
+    def test_set_should_fail_for_incorrect_values(self):
+        expected_name = True
+        expected_output_bit_depth = True
+        expected_output_sample_frequency = True
+        expected_on_modulation_frequency = True
+        expected_off_modulation_frequency = True
+        expected_input_bit_depth = True
+        expected_input_sample_frequency = True
+        expected_sublayer_height_mm = True
+        expected_laser_thickness_mm = True
+        expected_drips_per_mm = True
+        expected_max_deflection = True
+        expected_calibration_data = True
+        expected_calibration_scale = True
+        expected_draw_speed = True
+        expected_max_lead_distance_mm = True
+        expected_use_serial_zaxis = "!@#!@#"
+        expected_serial_port = True
+        expected_serial_on = True
+        expected_serial_off = True
+        expected_laser_offset = True
+        config = Configuration()
+
+        with self.assertRaises(Exception):
+            config.name = expected_name
+        with self.assertRaises(Exception):
+            config.audio.output.bit_depth = expected_output_bit_depth
+        with self.assertRaises(Exception):
+            config.audio.output.sample_rate = expected_output_sample_frequency
+        with self.assertRaises(Exception):
+            config.audio.output.modulation_on_frequency = expected_on_modulation_frequency
+        with self.assertRaises(Exception):
+            config.audio.output.modulation_off_frequency = expected_off_modulation_frequency
+        with self.assertRaises(Exception):
+            config.audio.input.bit_depth = expected_input_bit_depth
+        with self.assertRaises(Exception):
+            config.audio.input.sample_rate = expected_input_sample_frequency
+        with self.assertRaises(Exception):
+            config.options.sublayer_height_mm = expected_sublayer_height_mm
+        with self.assertRaises(Exception):
+            config.options.laser_thickness_mm = expected_laser_thickness_mm
+        with self.assertRaises(Exception):
+            config.dripper.drips_per_mm = expected_drips_per_mm
+        with self.assertRaises(Exception):
+            config.calibration.max_deflection = expected_max_deflection
+        with self.assertRaises(Exception):
+            config.calibration.data = expected_calibration_data
+        with self.assertRaises(Exception):
+            config.calibration.scale = expected_calibration_scale
+        with self.assertRaises(Exception):
+            config.options.draw_speed = expected_draw_speed
+        with self.assertRaises(Exception):
+            config.dripper.max_lead_distance_mm = expected_max_lead_distance_mm
+        with self.assertRaises(Exception):
+            config.serial.on = expected_use_serial_zaxis
+        with self.assertRaises(Exception):
+            config.serial.port = expected_serial_port
+        with self.assertRaises(Exception):
+            config.serial.on_command = expected_serial_on
+        with self.assertRaises(Exception):
+            config.serial.off_command = expected_serial_off
+        with self.assertRaises(Exception):
+            config.options.laser_offset = expected_laser_offset
 
 
 class ConfigurationManagerTests(unittest.TestCase,test_helpers.TestHelpers):
