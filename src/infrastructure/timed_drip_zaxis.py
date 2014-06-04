4debdfc465c62e495c88f8d66c481e53048fae6c
James Townley <software@peachyprinter.com>
2014-06-04 15:17:09 -0600

    Missed adding a file

---
+++ src/infrastructure/timed_drip_zaxis.py
@@ -0,0 +1,57 @@
+import threading
+import time
+import math
+from domain.zaxis import ZAxis
+
+class TimedDripZAxis(ZAxis, threading.Thread):
+    def __init__(self, 
+        drips_per_mm, 
+        call_back = None, 
+        calls_back_per_second = 15, 
+        drips_per_second = 1.0
+        ):
+        threading.Thread.__init__(self)
+        self._drips_per_mm = drips_per_mm
+        self._drips_per_second = drips_per_second
+        self.shutdown = False
+        self.running = True
+        self.start_time = 0
+        self._call_back = call_back
+        self._time_to_wait = 1.0 / (calls_back_per_second * 1.0)
+        self._drips_history = 0
+        self._height_history = 0.0
+
+    def set_call_back(self, call_back):
+        self._call_back = call_back
+
+    def set_drips_per_second(self, dps):
+        current_time = time.time() - self.start_time
+        drips = math.ceil(current_time * self._drips_per_second)
+        self._drips_history = self._drips_history + drips
+        self._height_history = self._height_history + (drips / self._drips_per_mm)
+        self.start_time = time.time()
+        self._drips_per_second = dps
+
+    def current_z_location_mm(self):
+        current_time = time.time() - self.start_time
+        height = (current_time * self._drips_per_second) / self._drips_per_mm
+        return self._height_history + height
+
+    def update_data(self):
+        if self._call_back:
+            current_time = time.time() - self.start_time
+            drips =  math.ceil(current_time * self._drips_per_second)
+            height = drips / self._drips_per_mm
+            self._call_back(self._drips_history + drips,self._height_history + height,self._drips_per_second)
+
+    def run(self):
+        self.start_time = time.time()
+        while self.running:
+            self.update_data()
+            time.sleep(self._time_to_wait)
+        self.shutdown = True
+
+    def stop(self):
+        self.running = False
+        while not self.shutdown:
+            time.sleep(0.1)
---
+++ test/infrastructure_test/timed_drip_zaxis_test.py
@@ -0,0 +1,110 @@
+import unittest
+import sys
+import os
+from mock import patch, Mock
+import time
+
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
+
+from infrastructure.timed_drip_zaxis import TimedDripZAxis
+
+class TimedDripZaxisTests(unittest.TestCase):
+    def setUp(self):
+        self.tdza = None
+        
+        self.calls = 0
+        self.drips = 0
+        self.height = 0
+        self.drips_per_second = 0
+    
+    def tearDown(self):
+        if self.tdza:
+            self.tdza.stop()
+
+    def call_back(self, drips, height, drips_per_second):
+        self.calls += 1
+        self.drips = drips
+        self.height = height
+        self.drips_per_second = drips_per_second
+
+    def test_shutsdown_cleanly(self):
+        self.tdza = TimedDripZAxis(1)
+        self.tdza.start()
+        self.tdza.stop()
+        self.assertFalse(self.tdza.is_alive())
+
+    def test_current_z_location_mm_returns_the_correct_number(self):
+        self.tdza = TimedDripZAxis(5, drips_per_second = 100)
+        expected_mm = 2
+        start = time.time()
+        self.tdza.start()
+        while time.time() - start < 0.1:
+            time.sleep(0.01)
+        result = self.tdza.current_z_location_mm()
+        self.tdza.stop()
+        self.assertAlmostEquals(expected_mm, result, places = 0)
+
+    def test_call_back_calls_back_at_correct_rate(self):
+        expected_time = 0.1
+        expected_drips = 10
+        expected_height = 10
+        expected_average = 100
+
+        self.tdza = TimedDripZAxis(
+            1, 
+            drips_per_second = expected_average, 
+            call_back = self.call_back, 
+            calls_back_per_second = 100
+            )
+
+        start = time.time()
+        self.tdza.start()
+        while self.calls < 10:
+            time.sleep(0.01)
+        actual_time = time.time() - start
+        self.tdza.stop()
+
+        self.assertAlmostEquals(expected_time, actual_time, places = 1)
+        self.assertEquals(expected_drips, self.drips)
+        self.assertAlmostEquals(expected_height, self.height, places = 0)
+        self.assertEquals(expected_average, self.drips_per_second)
+
+    def test_can_set_call_back(self):
+        self.tdza = TimedDripZAxis(
+            1, 
+            drips_per_second = 10, 
+            calls_back_per_second = 100
+            )
+
+        self.tdza.set_call_back(self.call_back) 
+        self.tdza.start()
+        time.sleep(0.1)
+        self.tdza.stop()
+
+        self.assertTrue(self.calls > 0 )
+
+    def test_can_change_drips_per_second_with_out_altering_existing_data(self):
+        self.tdza = TimedDripZAxis(
+            1, 
+            drips_per_second = 100, 
+            calls_back_per_second = 100
+            )
+
+        self.tdza.set_call_back(self.call_back) 
+        self.tdza.start()
+        time.sleep(0.1)
+        before_drips = self.drips
+        before_cb_height = self.height
+        before_height = self.tdza.current_z_location_mm()
+        self.tdza.set_drips_per_second(10)
+        time.sleep(0.1)
+        self.assertTrue(before_height <= self.tdza.current_z_location_mm(), "%s !<= %s" % (before_height, self.tdza.current_z_location_mm()))
+        self.assertTrue(before_cb_height <= self.height)
+        self.assertTrue(before_drips <= self.drips)
+        self.tdza.stop()
+
+        self.assertTrue(self.calls > 0 )
+
+if __name__ == '__main__':
+    unittest.main()
