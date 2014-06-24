a6fc4bc83a348a382a67e9601ee38df69ce9b79a
James Townley <software@peachyprinter.com>
2014-06-24 13:19:15 -0600

    implemented PeachyPrinter, Galvo, Mirror

--- src/infrastructure/transformer2.py
+++ src/infrastructure/transformer2.py
@@ -1,34 +1,78 @@
 import numpy as np
+from math import cos, sin,acos,asin, atan, tan, pi
+
 np.seterr(all='raise')
 
 class Laser(object):
     def __init__(self,laser_posisition, point_at):
-        self._laser_posisition=laser_posisition
-        self._point_at=point_at
+        self.laser_posisition=laser_posisition
+        self.point_at=point_at
 
     def _current_y_slope(self):
-        return  (self._laser_posisition[0,1] - self._point_at[0,1]) / (self._laser_posisition[0,2] - self._point_at[0,2]) 
+        return  (self.laser_posisition[0,1] - self.point_at[0,1]) / (self.laser_posisition[0,2] - self.point_at[0,2]) 
 
     def _current_x_slope(self):
-        return (self._laser_posisition[0,0] - self._point_at[0,0]) /(self._laser_posisition[0,2] - self._point_at[0,2])
+        return (self.laser_posisition[0,0] - self.point_at[0,0]) /(self.laser_posisition[0,2] - self.point_at[0,2])
 
     def fire(self,z):
-        x =  self._laser_posisition[0,0] - (self._laser_posisition[0,2]  - z) * self._current_x_slope()
-        y =  self._laser_posisition[0,1] - (self._laser_posisition[0,2]  - z) * self._current_y_slope()
-
+        x =  self.laser_posisition[0,0] - (self.laser_posisition[0,2]  - z) * self._current_x_slope()
+        y =  self.laser_posisition[0,1] - (self.laser_posisition[0,2]  - z) * self._current_y_slope()
         return np.matrix([x,y,z,1])
 
-
 class Mirror(object):
-    def __init__(self, base_point, normal):
+    def __init__(self, base_point, resting_normal, axis, galvo_pos):
+        #Although is is possible that the axis is not perpendicular to to normal NO!
         self._base_translation = np.append(np.matrix([[1.0, 0.0, 0.0, 0.0],[0.0,1.0,0.0, 0.0],[0.0,0.0,1.0, 0.0]]),base_point,0)
-        self._normal_unit = normal
+        self._resting_unit_normal = self._unit_vector(resting_normal)
+        self._unit_axis = self._unit_vector(axis)
+        self._galvo_pos = galvo_pos
+    
+    def _unit_vector(self,vector):
+            return vector / np.linalg.norm(vector)
 
     def _get_reflection(self):
-        return np.identity(4) - 2*np.outer(self._normal_unit,self._normal_unit)
+        return np.identity(4) - 2*np.outer(self._resting_unit_normal,self._resting_unit_normal)
 
-    def reflect(self, point):
-        base_point = point * np.linalg.inv(self._base_translation)
+    def _get_rotation_matrix(self, theta, axis):
+        x,y,z,w = axis.tolist()[0]
+        cos_theta = cos(theta)
+        sin_theta = sin(theta)
+        return np.matrix([
+            [x*x*(1-cos_theta)+cos_theta,      y*x*(1-cos_theta)-z*sin_theta,    z*x*(1-cos_theta)+y*sin_theta,  0],
+            [x*y*(1-cos_theta)+z*sin_theta,    y*y*(1-cos_theta)+cos_theta,      z*y*(1-cos_theta)-x*sin_theta,  0],
+            [x*z*(1-cos_theta)-y*sin_theta,    y*z*(1-cos_theta)+x*sin_theta,    z*z*(1-cos_theta)+cos_theta,    0],
+            [0,                0,                0,              1]
+            ])
+
+    def reflect(self, point, signal):
+        angle = self._galvo_pos(signal)
+        rotation_matrix = self._get_rotation_matrix(angle,self._unit_axis)
+
+        base_point = point * np.linalg.inv(self._base_translation)* np.linalg.inv(rotation_matrix)
         base_reflection = (base_point * self._get_reflection())
-        return (base_reflection * self._base_translation)
+        return (base_reflection * rotation_matrix * self._base_translation)
 
+class Galvo(object):
+    def __init__(self):
+        pass
+
+    def pos(self,signal):
+        return atan(signal)
+
+class PeachyPrinter(object):
+    def __init__(self,galvo1,galvo2,mirror1,mirror2,real_laser):
+        self._mirror1 = mirror1
+        self._mirror2 = mirror2
+        self._laser = real_laser
+
+    def write(self,deflection1, deflection2, real_height_z):
+        position_1 = self._mirror1.reflect(self._laser.laser_position, deflection1)
+        look_at_1 = self._mirror1.reflect(self._laser.point_at,deflection1)
+        phantom_laser1 = Laser(position_1,look_at_1)
+
+        position_2 = self._mirror2.reflect(phantom_laser1.laser_position, deflection2)
+        look_at_2 = self._mirror2.reflect(phantom_laser1.point_at, deflection2)
+        phantom_laser2 = Laser(position_2,look_at_2)
+
+        return phantom_laser2.fire(real_height_z)
+        
--- test/infrastructure_test/transformer2_test.py
+++ test/infrastructure_test/transformer2_test.py
@@ -1,13 +1,15 @@
 import unittest
 import numpy as np 
 import math
+from math import pi
 import os
 import sys
+from mock import MagicMock, patch
 
 sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
 
-from infrastructure.transformer2 import Laser, Mirror
+from infrastructure.transformer2 import *
 
 class TestHelp(object):
     epsilon = 0.000001
@@ -22,9 +24,13 @@ class MirrorTest(unittest.TestCase,TestHelp):
         point = np.matrix([0.0,0.0,2.0,1.0])
         expected_point = np.matrix([0.0,0.0,-2.0,1.0])
         normal =  np.matrix([0.0,0.0,1.0,0.0])
-        mirror = Mirror(base_point, normal)
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-        result = mirror.reflect(point)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
+
+        result = mirror.reflect(point, 0.0)
 
         self.assertProjectiveCoordinatesEqual(expected_point,result)
 
@@ -33,10 +39,28 @@ class MirrorTest(unittest.TestCase,TestHelp):
         point = np.matrix([1.0,0.0,3.0,1.0])
         expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
         normal = np.matrix([math.sqrt(2)/2,0.0,math.sqrt(2)/2,0.0])
+        axis =  np.matrix([-1.0,0.0,-1.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-        mirror = Mirror(base_point, normal)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
         
-        result = mirror.reflect(point)
+        result = mirror.reflect(point, 0.0)
+
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
+
+    def test_normal_can_be_non_unit(self):
+        base_point = np.matrix([0.0,0.0,0.0,1.0])
+        point = np.matrix([1.0,0.0,3.0,1.0])
+        expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
+        normal = np.matrix([1.0,0.0,1.0,0.0])
+        axis =  np.matrix([-1.0,0.0,-1.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
+
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
+        
+        result = mirror.reflect(point, 0.0)
 
         self.assertProjectiveCoordinatesEqual(expected_point,result)
 
@@ -45,9 +69,13 @@ class MirrorTest(unittest.TestCase,TestHelp):
         point = np.matrix([0.0,0.0,2.0,1.0])
         expected_point = np.matrix([0.0,0.0,-0.0,1.0])
         normal =  np.matrix([0.0,0.0,1.0,0.0])
-        mirror = Mirror(base_point, normal)
+        axis =  np.matrix([1.0,0.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-        result = mirror.reflect(point)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
+
+        result = mirror.reflect(point, 0.0)
 
         self.assertProjectiveCoordinatesEqual(expected_point,result)
 
@@ -56,20 +84,49 @@ class MirrorTest(unittest.TestCase,TestHelp):
         point = np.matrix([0.0,10.0,2.0,1.0])
         expected_point = np.matrix([0.0,10.0,-0.0,1.0])
         normal =  np.matrix([0.0,0.0,1.0,0.0])
-        mirror = Mirror(base_point, normal)
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-        result = mirror.reflect(point)
+        mirror = Mirror(base_point, normal,axis, galvo_pos)
+
+        result = mirror.reflect(point, 0.0)
 
         self.assertProjectiveCoordinatesEqual(expected_point,result)
+
+    def test_mirror_calculates_angle_correctly(self):
+        base_point = np.matrix([0.0,0.0,1.0,1.0])
+        normal =  np.matrix([1.0,0.0,0.0,0.0])
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        point = np.matrix([-1.0,0.0,1.0,1.0])
+        expected_point = np.matrix([0.0,0.0,2.0,1.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = math.pi / 4.0
+
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
+
+        result = mirror.reflect(point, 1.0)
+
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
+
+class GalvoTest(unittest.TestCase):
+    def test_galvo_pos(self):
+        galvo = Galvo()
+
+        self.assertAlmostEquals(-pi / 4.0, galvo.pos(-1.0),6)
+        self.assertAlmostEquals( pi / 4.0, galvo.pos(1.0),6)
+        self.assertAlmostEquals( 0.0     , galvo.pos(0.0),6)
+        self.assertAlmostEquals(-pi / 6.0, galvo.pos(-1 / math.sqrt(3)),6)
+        self.assertAlmostEquals( pi / 6.0, galvo.pos( 1 / math.sqrt(3)),6)
 
 class LaserTest(unittest.TestCase,TestHelp):
     def test_get_real_point_with_aimed_down(self):
         #Setup
         expected_target_point1 = np.matrix([0.0,0.0,1.0,1.0])
         expected_target_point2 = np.matrix([0.0,0.0,2.0,1.0])
-        laser_posisition = np.matrix([0.0,0.0,10.0,1.0])
+        laser_position = np.matrix([0.0,0.0,10.0,1.0])
         point_at = np.matrix([0.0,0.0,1.0,1.0])
-        laser = Laser(laser_posisition, point_at)
+        laser = Laser(laser_position, point_at)
         
         #Assert
         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
@@ -78,9 +135,9 @@ class LaserTest(unittest.TestCase,TestHelp):
     def test_get_real_point_aimed_somewhere(self):
         #Setup
         expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
-        laser_posisition = np.matrix([4.0,6.0,4.0,1.0])
+        laser_position = np.matrix([4.0,6.0,4.0,1.0])
         point_at = np.matrix([3.0,5.0,3.0,1.0])
-        laser = Laser(laser_posisition, point_at)
+        laser = Laser(laser_position, point_at)
         
         #Assert
         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
@@ -88,22 +145,77 @@ class LaserTest(unittest.TestCase,TestHelp):
     def test_get_real_point_with_moved_laser(self):
         #Setup
         expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
-        laser_posisition = np.matrix([4.0,6.0,4.0,1.0])
+        laser_position = np.matrix([4.0,6.0,4.0,1.0])
         point_at = np.matrix([3.0,5.0,3.0,1.0])
-        laser = Laser(laser_posisition, point_at)
+        laser = Laser(laser_position, point_at)
         
         #Assert
         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
 
     def test_should_throw_exception_when_laser_axis_is_parallel_to_point_at(self):
         #Setup
-        laser_posisition = np.matrix([0.0,0.0,1.0,1.0])
+        laser_position = np.matrix([0.0,0.0,1.0,1.0])
         point_at = np.matrix([1.0,0.0,1.0,1.0])
-        laser = Laser(laser_posisition, point_at)
+        laser = Laser(laser_position, point_at)
         with self.assertRaises(Exception):
             laser.fire(0.0)
+
+class PeachyPrinterTest(unittest.TestCase,TestHelp):
+    @patch('infrastructure.transformer2.Laser')
+    def test_write(self, mock_Laser):
+        #Setup
+        deflection1 = 1.0
+        deflection2 = -1.0
+        z_height = 10
+        laser_position = np.matrix([0.0,0.0,0.0,1.0])
+        laser_point_at = np.matrix([1.0,0.0,0.0,1.0])
+
+        phantom_laser1_position = np.matrix([1.0,0.0,7.0,1.0])
+        phantom_laser1_point_at = np.matrix([1.0,1.0,0.0,1.0])
+
+        phantom_laser2_position = np.matrix([2.0,0.0,0.0,1.0])
+        phantom_laser2_point_at = np.matrix([1.0,2.0,0.0,1.0])
+        
+        laser = MagicMock()
+        laser.laser_position = laser_position
+        laser.point_at = laser_point_at
+
+        mock_Laser.return_value.laser_position = phantom_laser1_position
+        mock_Laser.return_value.point_at = phantom_laser1_point_at
+
+        galvo1 = MagicMock()
+        galvo2 = MagicMock()
+
+        mirror1 = MagicMock()
+        mirror1_values = [phantom_laser1_position, phantom_laser1_point_at ] 
+        def mirror1_side_effect(a,b):
+            return mirror1_values.pop(0)
+        mirror1.reflect.side_effect = mirror1_side_effect
+
+        mirror2 = MagicMock()
+        mirror2_values = [phantom_laser2_position, phantom_laser2_point_at ] 
+        def mirror2_side_effect(a,b):
+            return mirror2_values.pop(0)
+        mirror2.reflect.side_effect = mirror2_side_effect
+
+        pp = PeachyPrinter(galvo1,galvo2,mirror1, mirror2, laser)
+
+        #Execute
+        pp.write(deflection1,deflection2, z_height)
+
+        #Assert
+        self.assertEquals((phantom_laser1_position, phantom_laser1_point_at), mock_Laser.mock_calls[0][1])
+        self.assertEquals((phantom_laser2_position, phantom_laser2_point_at), mock_Laser.mock_calls[1][1])
+        
+        self.assertEquals((laser_position, deflection1), mirror1.mock_calls[0][1])
+        self.assertEquals((laser_point_at, deflection1), mirror1.mock_calls[1][1])
+        
+        self.assertEquals((phantom_laser1_position, deflection2), mirror2.mock_calls[0][1])
+        self.assertEquals((phantom_laser1_point_at, deflection2), mirror2.mock_calls[1][1])
+        
+        mock_Laser.return_value.fire.assert_called_once_with(z_height)
+        
 
 
 if __name__ == '__main__':
     unittest.main()
-
