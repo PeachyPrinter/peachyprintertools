0c88b29525030e764a4efbed1d91e2ea88cf4bd4
James Townley <software@peachyprinter.com>
2014-06-27 16:04:47 -0600

    Bashed in homogenous transformer

--- src/infrastructure/transformer2.py
+++ src/infrastructure/transformer2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import *
 import sys,os
+import time
 
 sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
 
@@ -149,8 +150,10 @@ class PeachyPrinterFactory(object):
 
 class SpikeTransfomer(Transformer):
     def __init__(self, points):
-        self.calibration_points = points
-        self.go = False
+
+        self._scale = 1.0
+        self.MP = self._get_transform(points)
+
         monomial = [
             lambda x,y : x**3,  lambda x,y : x**2*y,   lambda x,y : x*y**2,   lambda x,y : y**3,
             lambda x,y : x**2,  lambda x,y : x*y,       lambda x,y : y**2,
@@ -160,33 +163,83 @@ class SpikeTransfomer(Transformer):
         
         xbend, ybend, coeffecient_vector_d1, coeffecient_vector_d2 = self._get_best_bends(points,monomial)
 
-        transform_d1 = lambda x,y : sum( [ m * a(self._x_tweak(x,xbend),self._y_tweak(y,ybend) ) for (m,a) in zip(coeffecient_vector_d1, monomial)] )
-        transform_d2 = lambda x,y : sum( [ m * a(self._x_tweak(x,xbend),self._y_tweak(y,ybend) ) for (m,a) in zip(coeffecient_vector_d2, monomial)] )
+        transform_d1 = lambda x,y : sum( [ m * a(self._x_de_pincushion(self._x_fit(x,y),xbend),self._y_de_pincushion(self._y_fit(x,y),ybend) ) for (m,a) in zip(coeffecient_vector_d1, monomial)] )
+        transform_d2 = lambda x,y : sum( [ m * a(self._x_de_pincushion(self._x_fit(x,y),xbend),self._y_de_pincushion(self._y_fit(x,y),ybend) ) for (m,a) in zip(coeffecient_vector_d2, monomial)] )
 
         self.transformit = lambda x,y : [ transform_d1(x,y), transform_d2(x,y) ]
-        self.go = True
+        
+        
+    def _get_transform(self, points):
+        return self._get_transformation_matrix(points)
+
+    def _get_transformation_matrix(self,mappings):
+        mapping_matrix = self._build_matrix(mappings)
+        b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        solved_matrix = np.linalg.solve(mapping_matrix,b)
+        forwards = np.matrix([solved_matrix[0:3], solved_matrix[3:6], solved_matrix[6:9]])
+        inverse = forwards.I
+        return inverse
+
+    def _build_matrix(self, points):
+        builder = []
+        index = 0
+        print("IN: %s" % points[:4])
+        for ((xi,yi),(xp,yp)) in points[:4]:
+            augment = self._augment(index,xi / self._scale, yi / self._scale)
+            builder.append([ xp, yp,  1,  0,  0,  0,  0,  0,  0] + augment[0])
+            builder.append([  0,  0,  0, xp, yp,  1,  0,  0,  0] + augment[1])
+            builder.append([  0,  0,  0,  0,  0,  0, xp, yp,  1] + augment[2])
+            index += 1
+        builder.append([  1,  1,  1,  1,  1,  1,  1,  1,  1,     0,   0,   0,   0])
+        return np.array(builder)
+
+    def _augment(self,index,xi,yi):
+        augment = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        for i in range(0,4):
+            if i == index:
+                augment[0][i] = -xi
+                augment[1][i] = -yi
+                augment[2][i] = -1
+        return augment
+
+    def no1_transform(self,(x,y)):
+        realworld = np.array([[x], [y], [1]])
+        computerland = self.MP * realworld
+        [kx, ky, k] = [computerland.item(i, 0) for i in range(3)]
+        x1,y1 = (kx/k, ky/k)
+        return (x1, y1)
+        if x1 >= -1.0 and x1 <= 1.0 and y1>= -1.0 and y1 <=1.0:
+            return (x1, y1)
+        else:
+            print("Boom: %s,%s ::+> %s,%s" % (x,y,x1, y1))
+
+    def _x_fit(self,x,y):
+        return self.no1_transform((x,y))[0]
+
+    def _y_fit(self,x,y):
+        return self.no1_transform((x,y))[1]
 
     def _get_best_bends(self,points,monomial):
         best_bend = (0, 0, 0 , 0,99999999999999999999999999999999999.0)
-        for y in range(0, 101):
-            for x in range(0, 101):
+        for y in range(-151, 151, 4):
+            for x in range(-151, 151, 4):
                 xbend = x / 100.0
                 ybend = y / 100.0
                 (coeffecient_vector_d1, error_d1), (coeffecient_vector_d2 , error_d2)= self._get_co_vectors(points, monomial,xbend,ybend )
                 error = error_d1 + error_d2
                 if best_bend[4] > error:
-                    best_bend = (x / 100.0, y / 100.0, coeffecient_vector_d1,coeffecient_vector_d2, error)
+                    best_bend = (x /  100.0, y / 100.0, coeffecient_vector_d1,coeffecient_vector_d2, error)
+        print("Best Bends: %s,%s" % (best_bend[0],best_bend[1]))
         return best_bend[:4]
 
     def _get_co_vectors(self, points, monomial, xbend,ybend):
         target_d1 = []
         target_d2 = []
         rows = []
-        for ((x,y),(d1,d2)) in self.calibration_points:
+        for ((x,y),(d1,d2)) in points:
             target_d1.append(d1)
             target_d2.append(d2)
-            rows.append([ f(self._x_tweak(x,xbend),self._y_tweak(y,ybend)) for f in monomial ])
-
+            rows.append([ f(self._x_de_pincushion(self._x_fit(x,y),xbend),self._y_de_pincushion(self._y_fit(x,y),ybend)) for f in monomial ])
 
         coeffecient_matrix = np.matrix(rows)
         target_vector_d1 = np.array(target_d1)
@@ -196,15 +249,15 @@ class SpikeTransfomer(Transformer):
         results_d2 = np.linalg.lstsq(coeffecient_matrix, target_vector_d2)[:2]
         return (results_d1, results_d2)
 
-    def _x_tweak(self,x,bend):
-        size = 50.0
-        offset=15.0
+    def _x_de_pincushion(self,x,bend):
+        size = 0.3
+        offset= 0.0
         scale = 3.0*size
         return bend * (scale*atan((x-offset)/scale)) + (1.0-bend) * x 
 
-    def _y_tweak(self,y,bend):
-        size = 50.0
-        offset=23.0
+    def _y_de_pincushion(self,y,bend):
+        size = 0.3
+        offset= 0.0
         scale = 3.0*size
         return bend * (scale*atan((y-offset)/scale)) + (1-bend) * y
 
@@ -216,8 +269,8 @@ class Spike(Tk):
     def __init__(self):
         Tk.__init__(self,None)
         self.ppf = PeachyPrinterFactory()
-        self.width = 1200
-        self.height = 800
+        self.width = 1000
+        self.height = 1000
         self.master = Tk()
         self.canvas = Canvas(self.master, width=self.width, height=self.height, background='black')
         self.canvas.pack()
@@ -227,20 +280,19 @@ class Spike(Tk):
         self.canvas.create_line(0, self.height/2, self.width ,self.height / 2, fill="white",width=1)
         self.canvas.create_line(self.width / 2, 0, self.width / 2 ,self.height, fill="white",width=1)
         self.pp = self.ppf.new_peachy_printer()
-        # self.pp = self.ppf.new_peachy_printer_with_err()
+        self.pp = self.ppf.new_peachy_printer_with_err()
         self.transformer = self._get_transformer(self.pp)
         self.show_approximations()
 
 
     def _get_transformer(self, printer):
         deflection_points = [
-        [-1.0, 1.0],[ 0.0, 1.0],[ 1.0, 1.0],[-1.0, 0.0],[ 0.0, 0.0],
-        [ 1.0, 0.0],[-1.0, -1.0],[ 0.0, -1.0],[ 1.0, -1.0],[0.5, 0.5],
-        [-0.3,0.2],[-0.1,0.2],[0.1,0.2],[0.3,0.2],
+        [ 1.0, 1.0],[-1.0, 1.0],[ 1.0,-1.0],[-1.0, -1.0],
+        [ 0.5, 0.5],[-0.3, 0.2],[-0.1, 0.2],[0.1,0.2],[0.3,0.2],
         [-0.3,0.0],[-0.1,0.0],[0.1,0.0],[0.3,0.0],
         [-0.3,-0.2],[-0.1,-0.2],[0.1,-0.2],[0.3,-0.2],
-
         ]
+
 
         # for point in deflection_points:
             # self.canvas.create_line(self.xscale(point[0]),self.yscale(point[1]),self.xscale(point[0]) + 1,self.yscale(point[1]) + 1,fill="green", width=5)
@@ -253,30 +305,32 @@ class Spike(Tk):
         return SpikeTransfomer(calibration_map)
 
     def xscale(self,x):
-        return x * (self.width / 2) + self.width  / 2 
+        return x * (self.width / 3) + self.width  / 2 
 
     def yscale(self,y):
-        return y * (self.height  / 2) + self.height  / 2
+        return y * (self.height  / 3) + self.height  / 2
 
     def show_approximations(self):
-        rangerx  = 12
-        rangery  = 12
-        xr = [x / (rangerx * 1.5)for x  in range(-rangerx,rangerx + 1)]
-        yr = [y / (rangery * 1.5) for y in range(-rangery,rangery + 1)]
+        bigness = 3
+        start = time.time()
+        rangerx  = 20
+        rangery  = 20
+        xr = [x / (rangerx * 1.3)for x  in range(-rangerx,rangerx + 1)]
+        yr = [y / (rangery * 1.3) for y in range(-rangery,rangery + 1)]
         for x in xr:
             for y in yr:
                 there = self.pp.write(x,y,-300).tolist()[0]
-                self.canvas.create_line(self.xscale(x),         self.yscale(y),         self.xscale(x) + 3,         self.yscale(y) + 3,         fill="blue", width=3)
+                self.canvas.create_line(self.xscale(x),         self.yscale(y),         self.xscale(x) + bigness,         self.yscale(y) + bigness,         fill="blue", width=bigness)
                 back = self.transformer.transform(there)
                 self.canvas.create_line(
                     there[0]+(self.width - 10)  / 2,
                     there[1] +(self.height - 10)  / 2,
-                    there[0] +1+(self.width - 10)  / 2,  
-                    there[1]+1+(self.height - 10)  / 2, 
+                    there[0] +1+(self.width - 10)  / 2,
+                    there[1]+1+(self.height - 10)  / 2,
                     fill="white", width=2)
                 # print('%s :=> %s' % (str((x,y)), back))
-                self.canvas.create_line(self.xscale(back[0]),   self.yscale(back[1]),   self.xscale(back[0]) + 3,   self.yscale(back[1]) + 3,   fill="red", width=3)
-
+                self.canvas.create_line(self.xscale(back[0]),   self.yscale(back[1]),   self.xscale(back[0]) + bigness,   self.yscale(back[1]) + bigness,   fill="red", width=bigness)
+        print("took: %s" % (time.time()- start))
 
 from Tkinter import *
 if __name__ == '__main__':
