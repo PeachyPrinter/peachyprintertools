007522c7dd4b6d39b27cb7ff38587c2f7eb1eec7
James Townley <software@peachyprinter.com>
2014-06-30 10:33:28 -0600

    Checked in acdentally commented out tests

--- test/infrastructure_test/simulator_test.py
+++ test/infrastructure_test/simulator_test.py
@@ -18,147 +18,147 @@ class TestHelp(object):
             diff = a[0,i]/a[0,3] - b[0,i]/b[0,3]
             self.assertTrue(abs(diff)<self.epsilon, 'Was out by : %s' % diff)
 
-# class MirrorTest(unittest.TestCase,TestHelp):
-#     def test_reflect_refects_at_origin_when_perpendicular_to_origin(self):
-#         base_point = np.matrix([0.0,0.0,0.0,1.0])
-#         point = np.matrix([0.0,0.0,2.0,1.0])
-#         expected_point = np.matrix([0.0,0.0,-2.0,1.0])
-#         normal =  np.matrix([0.0,0.0,1.0,0.0])
-#         axis =  np.matrix([0.0,1.0,0.0,0.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = 0
+class MirrorTest(unittest.TestCase,TestHelp):
+    def test_reflect_refects_at_origin_when_perpendicular_to_origin(self):
+        base_point = np.matrix([0.0,0.0,0.0,1.0])
+        point = np.matrix([0.0,0.0,2.0,1.0])
+        expected_point = np.matrix([0.0,0.0,-2.0,1.0])
+        normal =  np.matrix([0.0,0.0,1.0,0.0])
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-#         mirror = Mirror(base_point, normal, axis, galvo_pos)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
 
-#         result = mirror.reflect(point, 0.0)
+        result = mirror.reflect(point, 0.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-#     def test_reflect_refects(self):
-#         base_point = np.matrix([0.0,0.0,0.0,1.0])
-#         point = np.matrix([1.0,0.0,3.0,1.0])
-#         expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
-#         normal = np.matrix([math.sqrt(2)/2,0.0,math.sqrt(2)/2,0.0])
-#         axis =  np.matrix([-1.0,0.0,-1.0,0.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = 0
+    def test_reflect_refects(self):
+        base_point = np.matrix([0.0,0.0,0.0,1.0])
+        point = np.matrix([1.0,0.0,3.0,1.0])
+        expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
+        normal = np.matrix([math.sqrt(2)/2,0.0,math.sqrt(2)/2,0.0])
+        axis =  np.matrix([-1.0,0.0,-1.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-#         mirror = Mirror(base_point, normal, axis, galvo_pos)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
         
-#         result = mirror.reflect(point, 0.0)
+        result = mirror.reflect(point, 0.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-#     def test_normal_can_be_non_unit(self):
-#         base_point = np.matrix([0.0,0.0,0.0,1.0])
-#         point = np.matrix([1.0,0.0,3.0,1.0])
-#         expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
-#         normal = np.matrix([1.0,0.0,1.0,0.0])
-#         axis =  np.matrix([-1.0,0.0,-1.0,0.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = 0
+    def test_normal_can_be_non_unit(self):
+        base_point = np.matrix([0.0,0.0,0.0,1.0])
+        point = np.matrix([1.0,0.0,3.0,1.0])
+        expected_point = np.matrix([-3.0,0.0,-1.0,1.0])
+        normal = np.matrix([1.0,0.0,1.0,0.0])
+        axis =  np.matrix([-1.0,0.0,-1.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-#         mirror = Mirror(base_point, normal, axis, galvo_pos)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
         
-#         result = mirror.reflect(point, 0.0)
+        result = mirror.reflect(point, 0.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-#     def test_reflect_refects_not_at_origin_when_perpendicular_to_the_point(self):
-#         base_point = np.matrix([0.0,0.0,1.0,1.0])
-#         point = np.matrix([0.0,0.0,2.0,1.0])
-#         expected_point = np.matrix([0.0,0.0,-0.0,1.0])
-#         normal =  np.matrix([0.0,0.0,1.0,0.0])
-#         axis =  np.matrix([1.0,0.0,0.0,0.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = 0
+    def test_reflect_refects_not_at_origin_when_perpendicular_to_the_point(self):
+        base_point = np.matrix([0.0,0.0,1.0,1.0])
+        point = np.matrix([0.0,0.0,2.0,1.0])
+        expected_point = np.matrix([0.0,0.0,-0.0,1.0])
+        normal =  np.matrix([0.0,0.0,1.0,0.0])
+        axis =  np.matrix([1.0,0.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-#         mirror = Mirror(base_point, normal, axis, galvo_pos)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
 
-#         result = mirror.reflect(point, 0.0)
+        result = mirror.reflect(point, 0.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-#     def test_reflect_refects_not_at_origin_when_not_perpendicular_to_the_point(self):
-#         base_point = np.matrix([0.0,0.0,1.0,1.0])
-#         point = np.matrix([0.0,10.0,2.0,1.0])
-#         expected_point = np.matrix([0.0,10.0,-0.0,1.0])
-#         normal =  np.matrix([0.0,0.0,1.0,0.0])
-#         axis =  np.matrix([0.0,1.0,0.0,0.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = 0
+    def test_reflect_refects_not_at_origin_when_not_perpendicular_to_the_point(self):
+        base_point = np.matrix([0.0,0.0,1.0,1.0])
+        point = np.matrix([0.0,10.0,2.0,1.0])
+        expected_point = np.matrix([0.0,10.0,-0.0,1.0])
+        normal =  np.matrix([0.0,0.0,1.0,0.0])
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = 0
 
-#         mirror = Mirror(base_point, normal,axis, galvo_pos)
+        mirror = Mirror(base_point, normal,axis, galvo_pos)
 
-#         result = mirror.reflect(point, 0.0)
+        result = mirror.reflect(point, 0.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-#     def test_mirror_calculates_angle_correctly(self):
-#         base_point = np.matrix([0.0,0.0,1.0,1.0])
-#         normal =  np.matrix([1.0,0.0,0.0,0.0])
-#         axis =  np.matrix([0.0,1.0,0.0,0.0])
-#         point = np.matrix([-1.0,0.0,1.0,1.0])
-#         expected_point = np.matrix([0.0,0.0,2.0,1.0])
-#         galvo_pos = MagicMock()
-#         galvo_pos.return_value = math.pi / 4.0
+    def test_mirror_calculates_angle_correctly(self):
+        base_point = np.matrix([0.0,0.0,1.0,1.0])
+        normal =  np.matrix([1.0,0.0,0.0,0.0])
+        axis =  np.matrix([0.0,1.0,0.0,0.0])
+        point = np.matrix([-1.0,0.0,1.0,1.0])
+        expected_point = np.matrix([0.0,0.0,2.0,1.0])
+        galvo_pos = MagicMock()
+        galvo_pos.return_value = math.pi / 4.0
 
-#         mirror = Mirror(base_point, normal, axis, galvo_pos)
+        mirror = Mirror(base_point, normal, axis, galvo_pos)
 
-#         result = mirror.reflect(point, 1.0)
+        result = mirror.reflect(point, 1.0)
 
-#         self.assertProjectiveCoordinatesEqual(expected_point,result)
+        self.assertProjectiveCoordinatesEqual(expected_point,result)
 
-# class GalvoTest(unittest.TestCase):
-#     def test_galvo_pos(self):
-#         galvo = Galvo()
+class GalvoTest(unittest.TestCase):
+    def test_galvo_pos(self):
+        galvo = Galvo()
 
-#         self.assertAlmostEquals(-pi / 8.0 , galvo.pos(-1.0),6)
-#         self.assertAlmostEquals( pi / 8.0 , galvo.pos(1.0) ,6)
-#         self.assertAlmostEquals( 0.0      , galvo.pos(0.0) ,6)
-#         self.assertAlmostEquals(-pi / 16.0, galvo.pos(-0.5),6)
-#         self.assertAlmostEquals( pi / 16.0, galvo.pos( 0.5),6)
+        self.assertAlmostEquals(-pi / 8.0 , galvo.pos(-1.0),6)
+        self.assertAlmostEquals( pi / 8.0 , galvo.pos(1.0) ,6)
+        self.assertAlmostEquals( 0.0      , galvo.pos(0.0) ,6)
+        self.assertAlmostEquals(-pi / 16.0, galvo.pos(-0.5),6)
+        self.assertAlmostEquals( pi / 16.0, galvo.pos( 0.5),6)
 
-# class LaserTest(unittest.TestCase,TestHelp):
-#     def test_get_real_point_with_aimed_down(self):
-#         #Setup
-#         expected_target_point1 = np.matrix([0.0,0.0,1.0,1.0])
-#         expected_target_point2 = np.matrix([0.0,0.0,2.0,1.0])
-#         position = np.matrix([0.0,0.0,10.0,1.0])
-#         point_at = np.matrix([0.0,0.0,1.0,1.0])
-#         laser = Laser(position, point_at)
+class LaserTest(unittest.TestCase,TestHelp):
+    def test_get_real_point_with_aimed_down(self):
+        #Setup
+        expected_target_point1 = np.matrix([0.0,0.0,1.0,1.0])
+        expected_target_point2 = np.matrix([0.0,0.0,2.0,1.0])
+        position = np.matrix([0.0,0.0,10.0,1.0])
+        point_at = np.matrix([0.0,0.0,1.0,1.0])
+        laser = Laser(position, point_at)
         
-#         #Assert
-#         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
-#         self.assertProjectiveCoordinatesEqual(expected_target_point2, laser.fire(2.0))
+        #Assert
+        self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
+        self.assertProjectiveCoordinatesEqual(expected_target_point2, laser.fire(2.0))
 
-#     def test_get_real_point_aimed_somewhere(self):
-#         #Setup
-#         expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
-#         position = np.matrix([4.0,6.0,4.0,1.0])
-#         point_at = np.matrix([3.0,5.0,3.0,1.0])
-#         laser = Laser(position, point_at)
+    def test_get_real_point_aimed_somewhere(self):
+        #Setup
+        expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
+        position = np.matrix([4.0,6.0,4.0,1.0])
+        point_at = np.matrix([3.0,5.0,3.0,1.0])
+        laser = Laser(position, point_at)
         
-#         #Assert
-#         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
+        #Assert
+        self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
 
-#     def test_get_real_point_with_moved_laser(self):
-#         #Setup
-#         expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
-#         position = np.matrix([4.0,6.0,4.0,1.0])
-#         point_at = np.matrix([3.0,5.0,3.0,1.0])
-#         laser = Laser(position, point_at)
+    def test_get_real_point_with_moved_laser(self):
+        #Setup
+        expected_target_point1 = np.matrix([1.0,3.0,1.0,1.0])
+        position = np.matrix([4.0,6.0,4.0,1.0])
+        point_at = np.matrix([3.0,5.0,3.0,1.0])
+        laser = Laser(position, point_at)
         
-#         #Assert
-#         self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
+        #Assert
+        self.assertProjectiveCoordinatesEqual(expected_target_point1, laser.fire(1.0))
 
-#     def test_should_throw_exception_when_laser_axis_is_parallel_to_point_at(self):
-#         #Setup
-#         position = np.matrix([0.0,0.0,1.0,1.0])
-#         point_at = np.matrix([1.0,0.0,1.0,1.0])
-#         laser = Laser(position, point_at)
-#         with self.assertRaises(Exception):
-#             laser.fire(0.0)
+    def test_should_throw_exception_when_laser_axis_is_parallel_to_point_at(self):
+        #Setup
+        position = np.matrix([0.0,0.0,1.0,1.0])
+        point_at = np.matrix([1.0,0.0,1.0,1.0])
+        laser = Laser(position, point_at)
+        with self.assertRaises(Exception):
+            laser.fire(0.0)
 
 class PeachyPrinterTest(unittest.TestCase,TestHelp):
     @patch('infrastructure.simulator.Laser')
