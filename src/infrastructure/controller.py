f79d9c17f2660a8c4d993d29bf4ba033a74589c7
James Townley <software@peachyprinter.com>
2014-10-07 11:48:21 -0600

    Started work on refactor of the controller class

--- src/infrastructure/controller.py
+++ src/infrastructure/controller.py
@@ -118,6 +118,82 @@ class MachineStatus(object):
             'skipped_layers' : self._skipped_layers,
         }
 
+class Writer():
+    def __init__(self, 
+        override_speed,
+        state,
+        audio_writer,
+        path_to_audio,
+        laser_control
+        ):
+        self._override_speed = override_speed
+        self._state = state
+        self._audio_writer = audio_writer
+        self._path_to_audio = path_to_audio
+        self._laser_control = laser_control
+
+        self.laser_off_override = False
+        self._shutting_down = False
+        self._abort_current_command = False
+
+
+    def _almost_equal(self,a,b,sig_fig=5):
+        return ( a==b or int(a*10**sig_fig) == int(b*10**sig_fig))
+
+    def _same_posisition(self,pos_1,pos_2):
+        return self._almost_equal(pos_1[0],pos_2[0]) and self._almost_equal(pos_1[1],pos_2[1])
+
+    def process_layer(self, layer):
+        for command in layer.commands:
+            # logging.info("Processing command: %s" % command)
+            if self._shutting_down:
+                return
+            if self._abort_current_command:
+                self._abort_current_command = False
+                return
+            if type(command) == LateralDraw:
+                if not self._same_posisition(self._state.xy, command.start):
+                    self._move_lateral(command.start,layer.z,command.speed)
+                self._draw_lateral(command.end, layer.z, command.speed )
+            elif type(command) == LateralMove:
+                self._move_lateral(command.end, layer.z, command.speed)
+
+    def _move_lateral(self,(to_x,to_y), to_z,speed):
+        self._laser_control.set_laser_off()
+        for i in range(0,1):
+            self._write_lateral(to_x,to_y,to_z,speed)
+
+    def _draw_lateral(self,(to_x,to_y), to_z,speed):
+        if self.laser_off_override:
+            self._laser_control.set_laser_off()
+        else:
+            self._laser_control.set_laser_on()
+        for i in range(0,1):
+            self._write_lateral(to_x,to_y,to_z,speed)
+    
+    def _write_lateral(self,to_x,to_y, to_z,speed):
+        if self._override_speed and speed > self._override_speed:
+            speed = self._override_speed
+        to_xyz = [to_x,to_y,to_z]
+        path = self._path_to_audio.process(self._state.xyz,to_xyz , speed)
+        modulated_path = self._laser_control.modulate(path)
+        if self._audio_writer:
+            self._audio_writer.write_chunk(modulated_path)
+        self._state.set_state(to_xyz,speed)
+
+    def abort_current_command(self):
+        self._abort_current_command = True
+
+
+    def wait_till_time(self, wait_time):
+        while time.time() <= wait_time:
+            if self._shutting_down:
+                return
+            self._move_lateral(self._state.xy, self._state.z,self._state.speed)
+
+    def terminate(self):
+        self._shutting_down = True
+
 class Controller(threading.Thread,):
     def __init__(self, 
                     laser_control, 
@@ -146,21 +222,22 @@ class Controller(threading.Thread,):
         self.running = False
         self.starting = True
         self._shut_down = False
-
-        self.laser_off_override = False
         
         self._laser_control = laser_control
         self._path_to_audio = path_to_audio
         self._audio_writer = audio_writer
+
         self._layer_generator = layer_generator
         
         self.state = MachineState()
         self._status = MachineStatus(status_call_back)
+
+        self._writer = Writer(self._override_speed, self.state, self._audio_writer, self._path_to_audio, self._laser_control)
+
         self._zaxis = zaxis
         if self._zaxis:
             self._zaxis.set_call_back(self._status.drip_call_back)
 
-        self._abort_current_command = False
         self._pause = False
         self._pausing = False
         self._layer_start_command = layer_start_command
@@ -181,7 +258,7 @@ class Controller(threading.Thread,):
 
     def change_generator(self, layer_generator):
         self._pause = True
-        self._abort_current_command = True
+        self._writer.abort_current_command()
         while not self._pausing:
             logging.info("Waiting for pause point")
             time.sleep(0.01)
@@ -238,8 +315,8 @@ class Controller(threading.Thread,):
                 if self._should_process(ahead_by):
                     self._send_command(self._layer_start_command)
                     if self._pre_layer_delay:
-                        self._wait_till_time(time.time() + self._pre_layer_delay)
-                    self._process_layer(layer)
+                        self._writer.wait_till_time(time.time() + self._pre_layer_delay)
+                    self._writer.process_layer(layer)
                     self._send_command(self._layer_ended_command)
                 else:
                     logging.warning('Dripping too fast, Skipping layer')
@@ -264,68 +341,19 @@ class Controller(threading.Thread,):
             return True
         return False
 
-    def _almost_equal(self,a,b,sig_fig=5):
-        return ( a==b or int(a*10**sig_fig) == int(b*10**sig_fig))
-
-    def _same_posisition(self,pos_1,pos_2):
-        return self._almost_equal(pos_1[0],pos_2[0]) and self._almost_equal(pos_1[1],pos_2[1])
-
-    def _process_layer(self, layer):
-        for command in layer.commands:
-            # logging.info("Processing command: %s" % command)
-            if self._shutting_down:
-                return
-            if self._abort_current_command:
-                self._abort_current_command = False
-                return
-            if type(command) == LateralDraw:
-                if not self._same_posisition(self.state.xy, command.start):
-                    self._move_lateral(command.start,layer.z,command.speed)
-                self._draw_lateral(command.end, layer.z, command.speed )
-            elif type(command) == LateralMove:
-                self._move_lateral(command.end, layer.z, command.speed)
-
-    def _move_lateral(self,(to_x,to_y), to_z,speed):
-        self._laser_control.set_laser_off()
-        for i in range(0,1):
-            self._write_lateral(to_x,to_y,to_z,speed)
-
-    def _draw_lateral(self,(to_x,to_y), to_z,speed):
-        if self.laser_off_override:
-            self._laser_control.set_laser_off()
-        else:
-            self._laser_control.set_laser_on()
-        for i in range(0,1):
-            self._write_lateral(to_x,to_y,to_z,speed)
-    
-    def _write_lateral(self,to_x,to_y, to_z,speed):
-        if self._override_speed and speed > self._override_speed:
-            speed = self._override_speed
-        to_xyz = [to_x,to_y,to_z]
-        path = self._path_to_audio.process(self.state.xyz,to_xyz , speed)
-        modulated_path = self._laser_control.modulate(path)
-        if self._audio_writer:
-            self._audio_writer.write_chunk(modulated_path)
-        self.state.set_state(to_xyz,speed)
-
     def _wait_till(self, height):
         while self._zaxis.current_z_location_mm() < height:
             if self._shutting_down:
                 return
             self._status.set_waiting_for_drips()
-            self._move_lateral(self.state.xy, self.state.z,self.state.speed)
+            self._writer.wait_till_time(time.time() + (0.1))
         self._status.set_not_waiting_for_drips()
-
-    def _wait_till_time(self, wait_time):
-        while time.time() <= wait_time:
-            if self._shutting_down:
-                return
-            self._move_lateral(self.state.xy, self.state.z,self.state.speed)
 
 
     def _terminate(self):
         logging.info('Controller shutdown requested')
         self._shutting_down = True
+        self._writer.terminate()
         self._send_command(self._layer_ended_command)
         self._send_command(self._print_ended_command)
         try:
