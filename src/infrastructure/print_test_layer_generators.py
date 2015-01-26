32f6ed0dbd4ba66cb6b83a89026ab373a6687d2a
James Townley <software@peachyprinter.com>
2015-01-26 08:30:51 -0600

    Broke out layer generators for print test in to it's own file (fix)

--- src/infrastructure/print_test_layer_generators.py
+++ src/infrastructure/print_test_layer_generators.py
@@ -1,8 +1,12 @@
 import logging
 from domain.layer_generator import LayerGenerator
+from domain.commands import LateralDraw, Layer
+from math import pi, sin, cos
 
 
 class HalfVaseTestGenerator(LayerGenerator):
+    name = "Half Vase With A Twist"
+
     def __init__(self, height, radius, layer_height, speed=100):
         self._height = float(height)
         self._radius = float(radius)
@@ -23,7 +27,7 @@ class HalfVaseTestGenerator(LayerGenerator):
     def __next__(self):
         return self.next()
 
-    def points(self, radius, start_angle):
+    def _points(self, radius, start_angle):
         points = [[0, 0]]
         for step in range(0, self._steps_in_half):
             angle = start_angle + (step * self._rad_per_step)
@@ -33,12 +37,12 @@ class HalfVaseTestGenerator(LayerGenerator):
         points.append([0, 0])
         return points
 
-    def start_angle(self):
+    def _start_angle(self):
         percent_complete = self._current_height / self._height
         angle = pi * percent_complete
         return angle
 
-    def radius(self):
+    def _radius(self):
         percent_complete = self._current_height / self._height
         factor = (sin(percent_complete * 2.0 * pi * 2.0) + 1) / 2.0
         out = (self._radius * 0.75) + (factor * (self._radius * 0.25))
@@ -49,7 +53,7 @@ class HalfVaseTestGenerator(LayerGenerator):
         logging.info("Half vase current height: %s" % self._current_height)
         if self._current_height >= self._height:
             raise StopIteration
-        points = self.points(self.radius(), self.start_angle())
+        points = self._points(self._radius(), self._start_angle())
         commands = [LateralDraw(points[index - 1], points[index], self._speed) for index in range(1, len(points))]
         layer = Layer(self._current_height, commands=commands)
         self._current_height = self._current_height + self._layer_height
