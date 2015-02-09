990678c8f8fe919b689c50fa749fb20e095dc93b
James Townley <software@peachyprinter.com>
2015-02-09 15:48:19 -0600

    Added a twisty vase

--- src/infrastructure/print_test_layer_generators.py
+++ src/infrastructure/print_test_layer_generators.py
@@ -60,6 +60,64 @@ class HalfVaseTestGenerator(LayerGenerator):
         return layer
 
 
+class TwistVaseTestGenerator(LayerGenerator):
+    name = "Half Vase With A Bunch of Twists"
+
+    def __init__(self, height, width, layer_height, speed=100):
+        self._height = float(height)
+        self._max_radius = float(width) / 2.0
+        self._layer_height = float(layer_height)
+        self._speed = speed
+        self._current_height = 0.0
+        self._steps_in_half = 100
+        self._twists = 3 * pi
+        self._rad_per_step = pi / float(self._steps_in_half)
+        self._layers = self._height / self._layer_height
+
+        logging.info("Half vase height: %s" % self._height)
+        logging.info("Half vase radius: %s" % self._max_radius)
+        logging.info("Half vase layer height: %s" % self._layer_height)
+        logging.info("Half vase speed: %s" % self._speed)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def _points(self, radius, start_angle):
+        points = [[0, 0]]
+        for step in range(0, self._steps_in_half):
+            angle = start_angle + (step * self._rad_per_step)
+            x = sin(angle) * radius
+            y = cos(angle) * radius
+            points.append([x, y])
+        points.append([0, 0])
+        return points
+
+    def _start_angle(self):
+        percent_complete = self._current_height / self._height
+        angle = self._twists * percent_complete
+        return angle
+
+    def _radius(self):
+        percent_complete = self._current_height / self._height
+        factor = (sin(percent_complete * 2.0 * pi * 2.0) + 1) / 2.0
+        out = (self._max_radius * 0.75) + (factor * (self._max_radius * 0.25))
+        return out
+
+    def next(self):
+        logging.info("Half vase height: %s" % self._height)
+        logging.info("Half vase current height: %s" % self._current_height)
+        if self._current_height >= self._height:
+            raise StopIteration
+        points = self._points(self._radius(), self._start_angle())
+        commands = [LateralDraw(points[index - 1], points[index], self._speed) for index in range(1, len(points))]
+        layer = Layer(self._current_height, commands=commands)
+        self._current_height = self._current_height + self._layer_height
+        return layer
+
+
 class SimpleVaseTestGenerator(LayerGenerator):
     name = "Simple 5 Sided 180 Twist Vase (BETA)"
 
@@ -119,7 +177,7 @@ class LollipopTestGenerator(LayerGenerator):
         self._stick_radius = float(width) / 10.0
         remaining_height = height - self._base_height
         self._pop_radius = min(remaining_height / 2.0, float(width) / 2.0)
-        self._pop_center_height = ((height - self._base_height) / 2.0) + self._base_height
+        self._pop_center_height = height - self._pop_radius
         self._stick_complexity = 20
         self._pop_complexity = 100
 
@@ -150,11 +208,9 @@ class LollipopTestGenerator(LayerGenerator):
         return Layer(self._current_height, commands=commands)
 
     def _get_pop(self, current_height):
-        radius = 0
-        while radius < self._stick_radius:
-            rad_per_step = (2 * pi) / self._pop_complexity
-            distance_to_centre = abs(self._pop_center_height - current_height)
-            radius = sqrt((self._pop_radius * self._pop_radius) - (distance_to_centre * distance_to_centre))
+        rad_per_step = (2 * pi) / self._pop_complexity
+        distance_to_centre = abs(self._pop_center_height - current_height)
+        radius = sqrt((self._pop_radius * self._pop_radius) - (distance_to_centre * distance_to_centre))
         points = []
         for step in range(0, self._pop_complexity + 1):
             angle = step * rad_per_step
