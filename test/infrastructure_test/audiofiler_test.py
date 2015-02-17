56d29ec18a822cb542b6aa0a8147746f4dc651c9
James Townley <software@peachyprinter.com>
2015-02-17 08:48:52 -0600

    Pep8ing ahead of new feature

--- test/infrastructure_test/audiofiler_test.py
+++ test/infrastructure_test/audiofiler_test.py
@@ -3,12 +3,13 @@ import sys
 import os
 import numpy
 
-sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 
 from test_helpers import TestHelpers
 from infrastructure.audiofiler import PathToAudio
 from infrastructure.transformer import OneToOneTransformer, TuningTransformer
+
 
 class PathToAudioTests(unittest.TestCase, TestHelpers):
     transformer = OneToOneTransformer()
@@ -17,45 +18,45 @@ class PathToAudioTests(unittest.TestCase, TestHelpers):
         samples_per_second = 11
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
-        expected = numpy.array([[0.0,0.0],[0.25,0.0],[0.5,0.0],[0.75,0.0],[1.0,0.0]])
+        expected = numpy.array([[0.0, 0.0], [0.25, 0.0], [0.5, 0.0], [0.75, 0.0], [1.0, 0.0]])
 
-        actual = path2audio.process([0.0,0.0,1.0],[1.0,0.0,1.0],2.0)
+        actual = path2audio.process([0.0, 0.0, 1.0], [1.0, 0.0, 1.0], 2.0)
         self.assertNumpyArrayEquals(expected, actual)
 
     def test_given_an_y_movement_a_y_path_should_be_created(self):
         samples_per_second = 11
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
-        expected = numpy.array([[0.0,0.0],[0.0,0.25],[0.0,0.5],[0.0,0.75],[0.0,1.0]])
+        expected = numpy.array([[0.0, 0.0], [0.0, 0.25], [0.0, 0.5], [0.0, 0.75], [0.0, 1.0]])
 
-        actual = path2audio.process([0.0,0.0,1.0],[0.0,1.0,1.0],2.0)
+        actual = path2audio.process([0.0, 0.0, 1.0], [0.0, 1.0, 1.0], 2.0)
         self.assertNumpyArrayEquals(expected, actual)
 
     def test_given_an_xy_movement_na_xy_path_should_be_created(self):
         samples_per_second = 11
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
-        expected = numpy.array([[0.0,0.0],[0.25,0.25],[0.5,0.5],[0.75,0.75],[1.0,1.0]])
+        expected = numpy.array([[0.0, 0.0], [0.25, 0.25], [0.5, 0.5], [0.75, 0.75], [1.0, 1.0]])
 
-        actual = path2audio.process([0.0,0.0,1.0],[1.0,1.0,1.0],3.0)
+        actual = path2audio.process([0.0, 0.0, 1.0], [1.0, 1.0, 1.0], 3.0)
         self.assertNumpyArrayEquals(expected, actual)
 
     def test_given_an_non_zero_origin(self):
         samples_per_second = 11
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
-        expected = numpy.array([[1.0,1.0],[0.75,0.75],[0.5,0.5],[0.25,0.25],[0.0,0.0]])
+        expected = numpy.array([[1.0, 1.0], [0.75, 0.75], [0.5, 0.5], [0.25, 0.25], [0.0, 0.0]])
 
-        actual = path2audio.process([1.0,1.0,1.0],[0.0,0.0,1.0],3.0)
+        actual = path2audio.process([1.0, 1.0, 1.0], [0.0, 0.0, 1.0], 3.0)
         self.assertNumpyArrayClose(expected, actual)
 
     def test_given_a_command_with_no_change_assume_movment_and_time_equal_to_laser_width(self):
         samples_per_second = 4
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
-        expected = numpy.array([[0.0,0.0],[0.0,0.0]])
+        expected = numpy.array([[0.0, 0.0], [0.0, 0.0]])
 
-        actual = path2audio.process([0.0,0.0,1.0],[0.0,0.0,1.0], 1.0)
+        actual = path2audio.process([0.0, 0.0, 1.0], [0.0, 0.0, 1.0], 1.0)
         self.assertNumpyArrayClose(expected, actual)
 
     def test_can_change_transformer(self):
@@ -63,9 +64,9 @@ class PathToAudioTests(unittest.TestCase, TestHelpers):
         laser_size = 0.5
         path2audio = PathToAudio(samples_per_second, self.transformer, laser_size)
         path2audio.set_transformer(TuningTransformer())
-        expected = numpy.array([[1.0,1.0],[1.0,1.0]])
+        expected = numpy.array([[1.0, 1.0], [1.0, 1.0]])
 
-        actual = path2audio.process([1.0,1.0,1.0],[1.0,1.0,1.0], 1.0)
+        actual = path2audio.process([1.0, 1.0, 1.0], [1.0, 1.0, 1.0], 1.0)
         self.assertNumpyArrayClose(expected, actual)
 
 
