725d77ae8ae72d3bc7317b866e13a5ca50fbf09f
James Townley <software@peachyprinter.com>
2015-03-09 10:33:54 -0500

    Made example work better

--- src/example.py
+++ src/example.py
@@ -5,8 +5,11 @@ def callback(data):
 
 api = peachyprinter.PrinterAPI()
 printers = api.get_available_printers()
+if len(printers) == 0:
+	api.add_printer("Example Printer")
+	printers = api.get_available_printers()
 api.load_printer(printers[0])
-print_api = api.get_print_api(callback=callback)
+print_api = api.get_print_api(status_call_back=callback)
 test_print_api = api.get_test_print_api()
 test_print_name = test_print_api.test_print_names()
 test_print = test_print_api.get_test_print(test_print_name[0],10,10,0.1)
