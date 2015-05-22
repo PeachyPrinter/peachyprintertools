ec0e9b219e58e0163d4d37f307b550b188c6bf1c
James Townley <software@peachyprinter.com>
2015-05-22 09:46:33 -0600

    Upping loglevel for testing

--- src/peachyprinter/__init__.py
+++ src/peachyprinter/__init__.py
@@ -12,7 +12,7 @@ if os.name == 'nt':
     python_64 = sys.maxsize > 2**32
     if os.environ.get('PEACHY_API_DLL_PATH'):
         dep_path = os.environ.get('PEACHY_API_DLL_PATH')
-        logging.info("Loading usb dll via PEACHY_API_DLL_PATH")
+        logging.warning("Loading usb dll via PEACHY_API_DLL_PATH")
     else:
         try:
             dist = pkg_resources.get_distribution('PeachyPrinterToolsAPI')
@@ -20,17 +20,17 @@ if os.name == 'nt':
                 dep_path = os.path.join(dist,'peachyprinter' ,'dependancies', 'win', 'amd64')
             else:
                 dep_path = os.path.join(dist,'peachyprinter' ,'dependancies', 'win', 'x86')
-            logging.info("Loading usb dll via package resources")
+            logging.warning("Loading usb dll via package resources")
         except Exception:
             current_path = os.path.dirname(__file__)
             if python_64:
                 dep_path = os.path.join(current_path, '..','peachyprinter' ,'dependancies', 'win', 'amd64')
             else:
                 dep_path = os.path.join(current_path, '..','peachyprinter' ,'dependancies', 'win', 'x86')
-            logging.info("Loading usb dll via relitive path")
+            logging.warning("Loading usb dll via relitive path")
     dll_path = os.path.join(dep_path, 'libusb-1.0.dll')
 
-    logging.info("Attempting Loading usb dll from: %s" % dll_path)
+    logging.warning("Attempting Loading usb dll from: %s" % dll_path)
     try:
         ctypes.cdll.LoadLibrary(dll_path)
     except Exception as ex:
