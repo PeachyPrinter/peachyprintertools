4298ad61a2601c7d58156cc353b05938b8ab4bbc
James Townley <software@peachyprinter.com>
2015-06-22 09:22:09 -0600

    Added magic for detecting and prompting for install of c++ redist

--- src/peachyprinter/libraries.py
+++ src/peachyprinter/libraries.py
@@ -1,9 +1,27 @@
 import ctypes
 import pkg_resources
-from pkg_resources import DistributionNotFound    
+from pkg_resources import DistributionNotFound
 import os
 import sys
 import logging
+
+def ensure_windows_dependancies():
+    try:
+        from ctypes import cdll
+        cdll.msvcp120
+        print "success"
+    except WindowsError:
+        logging.error("Could not find MS c++ redist-brutal, will prompt for install")
+        import webbrowser
+        import win32api
+
+        win32api.MessageBox(0, 'Visual C++ Redistributable for Visual Studio 2012 is required to run. Your browser should open on microsofts download page', 'One more thing!')
+
+        url = "https://www.microsoft.com/en-ca/download/details.aspx?id=30679"
+        new = 2 # new tab
+        webbrowser.open(url, new=new)
+        raise EnvironmentError("Could not find MS c++ redist-brutal, will prompt for install")
+    return True
 
 def load_library(name):
     suffix = ''
@@ -15,6 +33,7 @@ def load_library(name):
         suffix = '.dylib'
         dependency_platform = 'mac'
     if os.name == 'nt':
+        ensure_windows_dependancies()
         suffix = '.dll'
         dependency_platform = 'win'
 
@@ -25,16 +44,16 @@ def load_library(name):
         try:
             dist = pkg_resources.get_distribution('PeachyPrinterToolsAPI')
             if python_64:
-                dll_path = os.path.join(dist,'peachyprinter' ,'dependancies', dependency_platform, 'amd64')
+                dll_path = os.path.join(dist, 'peachyprinter', 'dependancies', dependency_platform, 'amd64')
             else:
-                dll_path = os.path.join(dist,'peachyprinter' ,'dependancies', dependency_platform, 'x86')
+                dll_path = os.path.join(dist,'peachyprinter', 'dependancies', dependency_platform, 'x86')
             logging.info("Loading usb dll via package resources")
         except Exception:
             current_path = os.path.dirname(__file__)
             if python_64:
-                dll_path = os.path.join(current_path, '..','peachyprinter' ,'dependancies', dependency_platform, 'amd64')
+                dll_path = os.path.join(current_path, '..', 'peachyprinter', 'dependancies', dependency_platform, 'amd64')
             else:
-                dll_path = os.path.join(current_path, '..','peachyprinter' ,'dependancies', dependency_platform, 'x86')
+                dll_path = os.path.join(current_path, '..', 'peachyprinter', 'dependancies', dependency_platform, 'x86')
             logging.info("Loading usb dll via relitive path")
     
     dll_name = "%s%s" % (name, suffix)
