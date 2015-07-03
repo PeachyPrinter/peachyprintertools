82ff0857a4b2a32fa5ffafa2068397680012c262
James Townley <software@peachyprinter.com>
2015-07-03 14:05:17 -0600

    removed the affinity experiment

--- src/peachyprinter/__init__.py
+++ src/peachyprinter/__init__.py
@@ -2,13 +2,9 @@
 import os
 import sys
 import logging
-import affinity
 
-if sys.platform != 'darwin':
-    affinity.set_process_affinity_mask(0, 0x00000001)
-    
 logger = logging.getLogger('peachy')
-    
+
 from peachyprinter.api.peachy_printer_api import PrinterAPI
 from peachyprinter.infrastructure.communicator import MissingPrinterException
 
