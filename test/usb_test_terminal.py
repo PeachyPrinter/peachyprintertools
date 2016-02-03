ce09d60ece9f19eede6845e3687f9fa8309444f7
Harakan <wgt538@gmail.com>
2016-02-03 11:02:48 -0600

    Removed unused protobuf messages.

--- test/usb_test_terminal.py
+++ test/usb_test_terminal.py
@@ -8,7 +8,24 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 from peachyprinter.infrastructure.communicator import UsbPacketCommunicator
 from peachyprinter.infrastructure.messages import *
 
+    #ADC NUMBERS:
+    #0 - Vref Calibration Factor
+    #1 - 30C temperature calibration
+    #2 - 110C temperature calibration
+    #3 - ADC key (PA2)
+    #4 - Pin (PA3)
+    #5 - Temperature
+    #6 - Vref (3.3V volts)
+
 class UsbTestTerminal(object):
+    VREF_CAL_POS = 0
+    TEMP30_CAL_POS = 1
+    TEMP110_CAL_POS = 2
+    ADC_KEY_POS = 3
+    ADC_PA3_POS = 4
+    ADC_TEMP_POS = 5
+    ADC_VREF_POS = 6
+
     def __init__(self,verbose=False):
         self._verbose=verbose
 
@@ -20,6 +37,7 @@ class UsbTestTerminal(object):
         self._adcVal=[]
         self._dataRate=None
         self._adcCals=[]
+        self._move=[]
 
         self._usb = UsbPacketCommunicator(10)
         self._usb.register_handler(IAmMessage, self.i_am_handler)
@@ -33,6 +51,10 @@ class UsbTestTerminal(object):
     def usb_close(self):
         self._usb.close()
 
+    def move(self,x,y,laserPower):
+        self._move=[x,y,laserPower]
+        self._usb.send(MoveMessage(x,y,laserPower))
+
     def set_drips(self,dripCount=0):
         self._usb.send(SetDripCountMessage(dripCount))
 
@@ -41,6 +63,8 @@ class UsbTestTerminal(object):
 
     def enter_bootloader(self):
         self._usb.send(EnterBootloaderMessage())
+        if (self._verbose):
+            print "Bootloadereded"
 
     #A non-ideal push/pop queue interface.
     #Doesn't account for mis-matching - May be worth clearing on each request?
@@ -62,26 +86,25 @@ class UsbTestTerminal(object):
 
     def get_adc_calibrations(self):
         if len(self._adcCals)!=3:
-            [adcNum,adcVrefCal]=self.get_adc_val(0)
-            [adcNum,adcTemp30]=self.get_adc_val(1)
-            [adcNum,adcTemp110]=self.get_adc_val(2)
-            self._adcCals=[adcVrefCal,adcTemp30,adcTemp110]
+            [adcNum,adcVrefCal] = self.get_adc_val(self.VREF_CAL_POS)
+            [adcNum,adcTemp30] = self.get_adc_val(self.TEMP30_CAL_POS)
+            [adcNum,adcTemp110] = self.get_adc_val(self.TEMP110_CAL_POS)
+            self._adcCals = [adcVrefCal,adcTemp30,adcTemp110]
 
     def get_temperature(self):
 
         self.get_adc_calibrations()
 
         #Return actual Temperature in C
-        #Formula taken from STM32F0 datasheet page 252
-        adcVrefCal=self._adcCals[0]
-        adcTemp30=self._adcCals[1]
-        adcTemp110=self._adcCals[2]
+        #Formulas taken from STM32F0 datasheet page 252
+        adcVrefCal = self._adcCals[self.VREF_CAL_POS]
+        adcTemp30 = self._adcCals[self.TEMP30_CAL_POS]
+        adcTemp110 = self._adcCals[self.TEMP110_CAL_POS]
 
         #Get the current Vref and Temperature each time
-        [adcNum,adcTemperature]=self.get_adc_val(5)
-        [adcNum,adcVref]=self.get_adc_val(6)
+        [adcNum,adcTemperature]=self.get_adc_val(self.ADC_TEMP_POS)
+        [adcNum,adcVref]=self.get_adc_val(self.ADC_VREF_POS)
 
-        #... what a mess...
         vrefCompensation = 1.0*adcVrefCal/adcVref
         temperature = adcTemperature*vrefCompensation-adcTemp30
         temperature = temperature*(110-30)/(adcTemp110-adcTemp30)
@@ -95,8 +118,8 @@ class UsbTestTerminal(object):
     def get_supply_voltage(self):
 
         self.get_adc_calibrations()
-        adcVrefCal=self._adcCals[0]
-        [adcNum,adcVref]=self.get_adc_val(6)
+        adcVrefCal=self._adcCals[self.VREF_CAL_POS]
+        [adcNum,adcVref]=self.get_adc_val(self.ADC_VREF_POS)
 
         vrefCompensation = 1.0*adcVrefCal/adcVref
         
@@ -108,15 +131,9 @@ class UsbTestTerminal(object):
 
         return supplyVoltage
 
+    def get_adc_key_val(self):
+        return self.get_adc_val(self.ADC_KEY_POS)
 
-    #ADC NUMBERS:
-    #0 - Vref Calibration Factor
-    #1 - 30C temperature calibration
-    #2 - 110C temperature calibration
-    #3 - ADC key (PA2)
-    #4 - Pin (PA3)
-    #5 - Temperature
-    #6 - Vref (3.3V volts)
     def get_adc_val(self,adcNum):
         self._adcNum.append(adcNum)
         self._usb.send(GetAdcValMessage(adcNum))
@@ -148,28 +165,31 @@ class UsbTestTerminal(object):
             print('HW rev number: {0}'.format(message.hwrev))
             print('Data Rate:     {0}'.format(message.dataRate))
 
+def test_adc_key(t):
+    key_min=4095
+    key_max=0
+    while(1):
+        adcKeyVal = t.get_adc_key_val()
+        if adcKeyVal[1]>key_max:
+            key_max=adcKeyVal[1]
+        if adcKeyVal[1]<key_min:
+            key_min=adcKeyVal[1]
+
+        #print "Voltage: {0} V, Temperature {1} oC".format(voltage,temperature)
+        print "Adc Key Value: {0} Max: {1} Min: {2}".format(adcKeyVal,key_max,key_min)
+        time.sleep(1)
+
 if __name__ == '__main__':
-    t = UsbTestTerminal(verbose=True); 
+    t = UsbTestTerminal(verbose=False); 
     t.identify()
     t.set_drips(0)
-    #t.enter_bootloader()
-    while(1):
-        t.get_adc_val(0)
-        time.sleep(1)
-        t.get_adc_val(1)
-        time.sleep(1)
-        t.get_adc_val(2)
-        time.sleep(1)
-        t.get_adc_val(3)
-        time.sleep(1)
-        t.get_adc_val(4)
-        time.sleep(1)
-        t.get_adc_val(5)
-        time.sleep(1)
-        t.get_adc_val(6)
-        time.sleep(1)
-        t.get_temperature()
-        t.get_supply_voltage()
+    t.enter_bootloader()
+    #test_adc_key(t)
+    while(1): 
+        print "Voltage: {0} Temperature {1}".format(t.get_supply_voltage(), t.get_temperature())
+        adcKeyVal = t.get_adc_key_val()
+        print "Adc Key Value: {0}".format(adcKeyVal)
+        time.sleep(0.1)
 
 
 
